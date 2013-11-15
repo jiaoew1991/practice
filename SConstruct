602b33f8c5f1e930629351425ee8f13e652e56f2
LISH <lish516@126.com>
2013-11-15 14:28:22 +0800

    add log4cplus library

--- SConstruct
+++ SConstruct
@@ -11,15 +11,22 @@ currentPath = os.getcwd()
 
 ##scons.config包含不同工作平台的配置信息
 #    gmock_path:  gmock库的位置
+#    lib4cplus_path: lib4cplus库的路径
 
 config = json.load(open('./scons.config'))
 env.Append(CPPPATH=[currentPath])
 env.Append(CPPPATH=[os.path.join(config['gmock_path'], 'include'), config['gmock_path']])
 env.Append(CPPPATH=[os.path.join(config['gmock_path'], 'gtest/include'), os.path.join(config['gmock_path'], 'gtest')])
+env.Append(CPPPATH=[os.path.join(config['log4cplus_path'], 'include')])
+
+env.Append(LIBPATH=[os.path.join(config['log4cplus_path'], 'src/.libs')])
 
 libs = [
     File(os.path.join(config['gmock_path'], 'make/gmock_main.a')),
     File(os.path.join(config['gmock_path'], 'gtest/make/gtest_main.a')),
+    'log4cplus',
+    #File(os.path.join(config['log4cplus_path'], 'src/.libs/liblog4cplus.a')),
 ]
+env.Append(LIBS=['log4cplus'])
 
 env.SConscript('data_structure/SConscript', exports=['env', 'libs'], variant_dir='data_structure/build', duplicate=0)
--- data_structure/linkedlist_unittest.cpp
+++ data_structure/linkedlist_unittest.cpp
@@ -7,7 +7,20 @@
 
 #include "linkedlist.h"
 #include <gtest/gtest.h>
+#include <log4cplus/logger.h>
+#include <log4cplus/consoleappender.h>
+#include <log4cplus/layout.h>
+#include <log4cplus/ndc.h>
+#include <log4cplus/mdc.h>
+#include <log4cplus/helpers/loglog.h>
+#include <log4cplus/thread/threads.h>
+#include <log4cplus/helpers/sleep.h>
+#include <log4cplus/loggingmacros.h>
 #include <iostream>
+
+using namespace std;
+using namespace log4cplus;
+using namespace log4cplus::helpers;
 
 TEST(normalTest, testInt)
 {
@@ -73,6 +86,37 @@ TEST(outOfBundle, testInt)
 }
 int main(int argc, char *argv[])
 {
+    log4cplus::initialize ();
+    LogLog::getLogLog()->setInternalDebugging(true);
+
+    SharedObjectPtr<Appender> append_1(new ConsoleAppender());
+    append_1->setName(LOG4CPLUS_TEXT("First"));
+
+    log4cplus::getMDC ().put (LOG4CPLUS_TEXT ("key"),
+            LOG4CPLUS_TEXT ("MDC value"));
+    log4cplus::tstring pattern = LOG4CPLUS_TEXT("%d{%m/%d/%y %H:%M:%S,%Q} [%t] %-5p %c{2} %%%x%% - %X{key} - %m [%l]%n");
+    //	std::tstring pattern = LOG4CPLUS_TEXT("%d{%c} [%t] %-5p [%.15c{3}] %%%x%% - %m [%l]%n");
+    append_1->setLayout( std::auto_ptr<Layout>(new PatternLayout(pattern)) );
+    Logger::getRoot().addAppender(append_1);
+
+    Logger logger = Logger::getInstance(LOG4CPLUS_TEXT("test.a.long_logger_name.c.logger"));
+    LOG4CPLUS_DEBUG(logger, "This is the FIRST log message...");
+
+    sleep(1, 0);
+    {
+        NDCContextCreator ndc(LOG4CPLUS_TEXT("second"));
+        LOG4CPLUS_INFO(logger, "This is the SECOND log message...");
+    }
+
+    sleep(1, 0);
+    LOG4CPLUS_WARN(logger, "This is the THIRD log message...");
+
+    sleep(1, 0);
+    LOG4CPLUS_ERROR(logger, "This is the FOURTH log message...");
+
+    sleep(1, 0);
+    LOG4CPLUS_FATAL(logger, "This is the FOURTH log message...");
+
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
